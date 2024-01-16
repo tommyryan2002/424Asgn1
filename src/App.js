796642e1ac42cee0d7d5f08a03d308354ad8e03f
Thomas Ryan <tommyryan2002@gmail.com>
2024-01-15 18:03:02 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { Home } from "./Home";
+import { Landing } from "./Landing";
+import React, { useState } from "react";
 
-function App() {
+import { ProtectedRoute } from "./utils/ProtectedRoute";
+import { fakeAuth } from "./utils/FakeAuth";
+import { NavLink } from 'react-router-dom';
+
+import { useAuth } from "./context/AuthProvider";
+import { AuthProvider } from "./context/AuthProvider";
+
+export const AuthContext = React.createContext(null);  // we will use this in other components
+
+const App = () => {
+  const [token, setToken] = React.useState(null);
+  const [user, setUser] = React.useState(null);
+  const handleLogin = async () => {
+    const token = await fakeAuth();
+    setToken(token);
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+  };
+return (
+  <AuthProvider>
+    <Navigation />
+   
+    <h1>React Router</h1>
+
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="landing" element={<Landing />} />
+      <Route path="home" element={<Home />} />
+      <Route path="*" element={<p>There's nothing here: 404!</p>} />
+    </Routes>
+  </AuthProvider>
+);
+};
+
+const Navigation = () => {
+  const { value } = useAuth();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <nav>
+      <NavLink to="/home">Home</NavLink>
+      <NavLink to="/landing">Landing</NavLink>
+      {value.token && (
+        <button type="button" onClick={value.onLogout}>
+          Sign Out
+      </button>
+    )}
+  </nav>
+);
 }
 
 export default App;
